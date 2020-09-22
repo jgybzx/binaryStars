76c2a1861b7f93029efcffa67e22255a2a3850eb
guojy <guojy_6526@163.com>
2020-09-22 09:36:47 +0800

    list的Stream流，排序方法

--- binary-remote/src/main/java/com/jgybzx/controller/FileUploadController.java
+++ binary-remote/src/main/java/com/jgybzx/controller/FileUploadController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * @author jgybzx
@@ -24,5 +28,29 @@ public class FileUploadController {
     @PostMapping("fileUpload")
     public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
         return fileUpload.fileUpload(file);
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        for (String s : list) {
+            System.out.println(s);
+        }
+        remove1(list);
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+    }
+
+    private static void remove1(List<String> list) {
+        for (String s : list) {
+            if ("3".equals(s)){
+                list.remove(s);
+            }
+        }
     }
 }
