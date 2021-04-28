479b3f87d31f9d6005d8ec8ba459a724f7c486d1
guojy <guojy_6526@163.com>
2021-04-28 15:37:23 +0800

    mybatis 实体类和mapper生成工具类及配置

--- binary-student/src/main/java/com/jgybzx/controller/StudentController.java
+++ binary-student/src/main/java/com/jgybzx/controller/StudentController.java
@@ -92,11 +92,13 @@ public class StudentController {
     }
 
     public static void main(String[] args) {
+
         String s = "1.2.3";
         String[] split = s.split("\\.");
         for (int i = 0; i < split.length; i++) {
             System.out.println(split[i] + "asasd");
         }
     }
+    // 分支冲突测试
 }
 
