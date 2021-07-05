508e2c76c7f7841ccbc29d3329b582ca741717a7
guojy <guojy_6526@163.com>
2021-07-05 14:35:44 +0800

    处理阿里Accesskey泄密，使用base64加密

--- binary-student/src/main/java/com/jgybzx/controller/ExcelDownloadController.java
+++ binary-student/src/main/java/com/jgybzx/controller/ExcelDownloadController.java
@@ -2,6 +2,7 @@ package com.jgybzx.controller;
 
 import com.alibaba.excel.write.metadata.style.WriteCellStyle;
 import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
+import com.alibaba.fastjson.JSON;
 import com.jgybzx.JsonUtil;
 import com.jgybzx.interceptor.CustomHandler;
 import com.jgybzx.model.Customer;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.alibaba.excel.EasyExcelFactory.write;
 
@@ -30,39 +33,45 @@ import static com.alibaba.excel.EasyExcelFactory.write;
 @RequestMapping("excel")
 public class ExcelDownloadController {
     /**
-     * 文件下载（失败了会返回一个有部分数据的Excel）
-     * <p>
-     * 1. 创建excel对应的实体对象 参照{}
-     * <p>
-     * 2. 设置返回的 参数
-     * <p>
-     * 3. 直接写，这里注意，finish的时候会自动关闭OutputStream,当然你外面再关闭流问题不大
+     * excle下载
+     *
+     * @param response
+     * @author jgybzx
+     * @date 2021/6/29 17:52
      */
     @GetMapping("download")
     public void download(HttpServletResponse response) throws IOException {
-        List<Customer> customerList = JsonUtil.jsonToList(BasicConstants.easyExcelString, Customer.class);
-        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        response.setCharacterEncoding("utf-8");
-        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String transformDate = simpleDateFormat.format(new Date());
-        // TODO: 2021/6/29  接口响应文件名中文乱码，待修改
-        String fileName = "咨询人查询" + transformDate;
-        //避免文件名中文乱码，将UTF8打散重组成ISO-8859-1编码方式
-        fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
-        response.setHeader("Content-Disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
-        // 标题样式
-        WriteCellStyle headWriteCellStyle = EasyExcelUtils.getHeadStyle();
-        // 内容样式
-        WriteCellStyle contentWriteCellStyle = EasyExcelUtils.getContentStyle();
-        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
-        CustomHandler customHandler = new CustomHandler(headWriteCellStyle, contentWriteCellStyle);
-        write(response.getOutputStream(), Customer.class)
-                .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy())
-                .registerWriteHandler(customHandler)
-                .sheet("信息").doWrite(customerList);
-
+        try {
+            int i = 1 / 0;
+            List<Customer> customerList = JsonUtil.jsonToList(BasicConstants.easyExcelString, Customer.class);
+            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            response.setCharacterEncoding("utf-8");
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            String transformDate = simpleDateFormat.format(new Date());
+            String fileName = "咨询人查询" + transformDate;
+            fileName = URLEncoder.encode(fileName, "UTF-8").replaceAll("\\+", "%20");
+            response.setHeader("Content-Disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
+            // 标题样式
+            WriteCellStyle headWriteCellStyle = EasyExcelUtils.getHeadStyle();
+            // 内容样式
+            WriteCellStyle contentWriteCellStyle = EasyExcelUtils.getContentStyle();
+            // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
+            CustomHandler customHandler = new CustomHandler(headWriteCellStyle, contentWriteCellStyle);
+            write(response.getOutputStream(), Customer.class)
+                    .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy())
+                    .registerWriteHandler(customHandler)
+                    .sheet("信息").doWrite(customerList);
+        } catch (Exception e) {
+            response.reset();
+            response.setStatus(500);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("status", "failure");
+            map.put("message", "下载文件失败" + e.getMessage());
+            response.getWriter().println(JSON.toJSONString(map));
+//            throw new BaseException("500", "表格下载失败：" + e.getMessage());
+        }
     }
 
 }
